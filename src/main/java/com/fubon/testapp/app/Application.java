f772b31a6680f5da0e3b11e3050386e08aa392b7
sclin0323 <sclin0323@gmail.com>
2020-02-10 15:33:52 +0800

    test eclipse commit

--- src/main/java/com/fubon/testapp/app/Application.java
+++ src/main/java/com/fubon/testapp/app/Application.java
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = { "com.fubon.testapp.repository" })
 public class Application {
 
+	
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
