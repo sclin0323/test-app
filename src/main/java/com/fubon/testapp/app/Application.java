a534b3782bbf08d7faec521cf072eb759b03ba56
sclin0323 <sclin0323@gmail.com>
2020-02-12 11:42:26 +0800

    update test case

--- src/main/java/com/fubon/testapp/app/Application.java
+++ src/main/java/com/fubon/testapp/app/Application.java
@@ -17,7 +17,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = { "com.fubon.testapp.repository" })
 public class Application {
 
-	
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
