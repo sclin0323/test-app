8621dfdc20220658e2d014f4a270f8545e54ae15
sclin0323 <sclin0323@gmail.com>
2020-02-12 15:53:43 +0800

    update rest template

--- src/test/java/com/fubon/testapp/test/RestTemplateTest.java
+++ src/test/java/com/fubon/testapp/test/RestTemplateTest.java
@@ -5,8 +5,11 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
+import com.fubon.testapp.api.res.PharmacieRes;
 import com.fubon.testapp.service.RoleServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,6 +21,21 @@ class RestTemplateTest {
 	@Test
 	void test() {
 		log.info("xxxxxxxxxxxxx");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		
+		log.info("response: "+response);
+		
+//		RestTemplate restTemplate = new RestTemplate();
+//		String url = "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+//		ResponseEntity<PharmacieRes> response = restTemplate.getForEntity(url, PharmacieRes.class);
+//		
+//		PharmacieRes o = response.getBody();
+//		
+//		log.info("Type: "+o.getType());
+//		log.info("Size: "+o.getFeatures().size());
 	}
 
 }
